f21f6ef55073c258d38231d06126d80248200aa0
Jason Ban Tze Tan <jtbt1999@go.byuh.edu>
2022-12-16 17:02:05 -1000

    done assignment

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+// 1) Create a new Flutter App (in this project) and output an AppBar and some _text
+// below it
+// 2) Add a button which changes the _text (to any other _text of your choice)
+// 3) Split the app into three widgets: App, TextControl & Text
+
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final _text = [
+      'I\'m happy!',
+      'I\'m sad.',
+      'I\'m angry!!'
+    ];
+
+  var _textIndex = 0;
+
+  void changeText() {
+    setState(() {
+      _textIndex = _textIndex + 1;
+    });
+    print(_textIndex);
+    if(_textIndex < _text.length) {
+      print('Click for more!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Assignment App'),
+        ),
+        body: _textIndex < _text.length
+        ? Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            ElevatedButton(
+              onPressed: changeText, 
+              child: Text('Click here to change the display _text')
+            ),
+            Text(_text[_textIndex]),
+          ],
+        )
+        : Text('Done!'),
+      ),
+    );
+  }
+}
